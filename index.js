7e9bd2c316201b5ebdb5ef73a305396f72da3eb3
JeanSimon20 <jean.simon@vallegrande.edu.pe>
2022-06-14 12:11:53 -0500

    Subiendo Cambios

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/persona', personaRoute);
 
 app.get('/', (req, res) => {
-  res.send("Vallegrande");
+  res.send("Jean Carlos");
 });
 
 app.listen(port, () => {
